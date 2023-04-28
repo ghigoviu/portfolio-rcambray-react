0b88148287cd61689d73551883b252502987da7d
Rodrigo Ronaldo Cambray Barrera <rrcambrayb@capufe.gob.mx>
2023-04-28 17:09:25 -0600

    Creado proyecto de portafolio para rcambray

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "bulma/css/bulma.min.css";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+
+    
+
+    </div>
+  );
+}
+
+export default App;
